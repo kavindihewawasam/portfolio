1650d6dd4b571073805521ccee517e1ddeba1aad
kavindihewawasam <kwathsalah@gmail.com>
2024-11-17 20:29:23 +0530

    added more css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,27 @@
 import React from "react";
 import "./styles/Main.css";
+import { Facebook, Instagram, LinkedIn, Twitter } from "@mui/icons-material";
 
 const Main = () => {
-  return <div></div>;
+  return (
+    <div className="main">
+      <div className="main_container">
+        <div className="main_content">
+          <div className="text">
+            <p>Hello Everyone !</p>
+            <h1>I'm Kavindi Hewawasam</h1>
+            <p>Front-end Developer & Designer</p>
+            <div className="icons">
+              <Twitter className="icon" />
+              <Instagram className="icon" />
+              <Facebook className="icon" />
+              <LinkedIn className="icon" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Main;
