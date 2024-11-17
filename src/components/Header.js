10418639889b73c02bf2fc870ac859df027fa14b
kavindihewawasam <kwathsalah@gmail.com>
2024-11-17 15:19:17 +0530

    updated header.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Header.css";
+import { Close, MenuBookOutlined } from "@mui/icons-material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-  return <div></div>;
+  const { active, setActive } = useState(false);
+
+  const showMenu = () => {
+    setActive(!active);
+  };
+
+  return (
+    <div className="header">
+      <div className="header_logo">
+        <h1>simplilearn</h1>
+      </div>
+
+      <nav>
+        <ul>
+          <div className="closed">
+            <Close className="close" onclick={showMenu} />
+          </div>
+
+          <li>
+            <Link to="/">
+              <b>Home</b>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <b>About</b>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <b>Portfolio</b>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <b>Blog</b>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+              <b>Contact</b>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="changer">
+        <MenuBookOutlined className="menu" onclick={showMenu} />
+      </div>
+    </div>
+  );
 };
 
 export default Header;
