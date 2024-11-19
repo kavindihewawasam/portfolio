58b0dae79f8c2a3ce8b87d8f892250a86dc14f60
kavindihewawasam <kwathsalah@gmail.com>
2024-11-19 22:41:43 +0530

    created section.js and contact.js. themimport them in to app.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Section from "./components/Section";
+import Contact from "./components/Contact";
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Section />
+      <Contact />
     </div>
   );
 }
